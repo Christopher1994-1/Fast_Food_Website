95f57f15bb97a89f53140cf40c838c93bfa17f52
CEJ Van Niekirk <cejvanniekirk098@gmail.com>
2023-03-19 23:23:03 -0600

    initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+# page for index
+@app.route('/')
+def index():
+    render_template('index.html')
+    
+    
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
